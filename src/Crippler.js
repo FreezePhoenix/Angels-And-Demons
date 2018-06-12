8ab1987002717d9cdd7d0da301d39bb6e5f04372
FreezePhoenix <683264@sylvaniastudents.org>
2018-06-12 15:15:47 -0400

    Made some changes to the UI. Also added Crippler classes.

--- src/Crippler.js
+++ src/Crippler.js
@@ -1,6 +1,6 @@
 class Crippler extends Card {
   constructor(hand, deck, name, effects, maxHealth, summonCost) {
-    super(maxHealth, "N/A", "Silver", 9, name, true, hand, deck, "N/A");
+    super(maxHealth, "N/A", "#C0C0C0", 9, name, true, hand, deck, "N/A");
     this.summonCost = summonCost || 0;
     this.effects = effects;
   };
@@ -16,7 +16,7 @@ class DarkAgent extends Crippler {
 };
 class Lamp extends Crippler {
   constructor(hand=enemyHand, deck=enemyDeck) {
-    super(hand, deck, "Crippler", {
+    super(hand, deck, "Lamp", {
       manaCost: new Effect(3, 3, "manaCost", 1),
       health: new Effect(-10, 3, "health", -5),
       attack: new Effect(-3, 3, "attack", -5)
