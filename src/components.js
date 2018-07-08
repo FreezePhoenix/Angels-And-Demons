20bc4bfaea3853fe726d35b5261f5075a1ecaa51
FreezePhoenix <683264@sylvaniastudents.org>
2018-07-08 23:45:42 +0000

    Graphics are now smoother, cards slide back when a card is discarded or removed

--- src/components.js
+++ src/components.js
@@ -1,17 +1,17 @@
 Vue.component('card', {
-  'template': `<div class="card" :key="item.UUID" :id="item.UUID" v-if="item.name !== null" :style="{
+  'template': `<div class="card" :data-health="''+Boolean((item.health || 0) && !item.inHand)" :key="item.UUID" :id="item.UUID" v-if="item.name !== null" :style="{
                     'border-radius': '4px',
                     'background-color': item.selected ? 'LightGrey' : 'transparent'
                     }"> 
                    <div class="name">
+                     <div class="summonOrAttack"> </div>
                      <div class="tag" :style="{
                      'cursor': !item.locked && !gameOver && !item.used && ( (item.inHand ? item.summonCost : item.manaCost) <= item.manaManager.mana) && (item.isDecksTurn) && (item.inHand ? true : !item.isLand) ? 'pointer' : 'default',
                      'text-shadow': item.selected ? ('0 0 4px' + item.nameColor) : 'none',
-                     'width': '100px',
                      'color': (item.locked  || item.used || gameOver || ( (item.inHand ? item.summonCost : item.manaCost) > item.manaManager.mana) || (item.isDecksTurn) && (item.inHand ? false : item.inHand)) ? (colors.DarkerColors[item.nameColor]) : ((item.selected) ? (colors.LighterColors[item.nameColor]) : (item.nameColor))}" align="center" :onclick=" gameOver ? void(0) : ((item.indexInDecks ? 'enemy' : 'player') + (item.inHand ? 'Hand' : 'Deck')+ '.cards['+item.ID+']' + (item.inHand ? '.summon()' : '.onclick()'))">
                          {{ item.name }}
                    </div>
-                   <div class="discard" :style="{'cursor': item.isDecksTurn ? 'pointer' : 'default' }" :onclick="(item.indexInDecks ? 'enemy' : 'player') + (item.inHand ? 'Hand' : 'Deck')+ '.cards['+item.ID+']' + '.discard()'"> D </div>
+                   <div class="discard" :style="{'cursor': item.isDecksTurn ? 'pointer' : 'default'}" :onclick="(item.indexInDecks ? 'enemy' : 'player') + (item.inHand ? 'Hand' : 'Deck')+ '.cards['+item.ID+']' + '.discard()'"> D </div>
                  </div> 
                  <br v-if="item.health && !item.inHand"> 
                  <div v-if="item.health && !item.inHand" style="background-color:black; width:100px;" class="health"> 
